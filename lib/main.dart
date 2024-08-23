fde900b2704d6f9c47b98ddc2ebcf932f9e1cd2a
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-23 16:33:31 +0300

    init app. add home view.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:movies_app/home/home_view.dart';
+
+void main(){
+  runApp(const MoviesApp());
+}
+class MoviesApp extends StatelessWidget {
+  const MoviesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeView.routeName,
+      routes: {
+        HomeView.routeName:(context)=>const HomeView(),
+      },
+    );
+  }
+}
