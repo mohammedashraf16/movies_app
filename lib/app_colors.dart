58c0ed81a0d19e5db732f46cc4b7573c5f240818
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-23 17:38:33 +0300

    add assets. add app colors.

---
+++ lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+abstract class AppColors{
+  static const Color primaryColor =Color(0xff121312);
+  static const Color secondaryColor =Color(0xff1A1A1A);
+  static const Color yellowColor =Color(0xffFFBB3B);
+  static const Color whiteColor =Color(0xffFFFFFF);
+  static const Color grayColor =Color(0xffB5B4B4);
+}
