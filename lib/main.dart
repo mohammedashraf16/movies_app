81f64796cf12b6acff154c7ee434e992ec524485
mohammedashraf16 <mohammedashraf1692003@gmail.com>
2024-08-23 17:43:21 +0300

    add assets class& generate features directory

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/home/home_view.dart';
+import 'package:movies_app/features/home/home_view.dart';
 
 void main(){
   runApp(const MoviesApp());
